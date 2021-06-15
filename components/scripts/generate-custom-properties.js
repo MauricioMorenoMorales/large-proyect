87baa236ae38644ad5246db0bb1205afbf5ef90d
mauriciomoreno <mauriciomorenomorales@protonmail.ch>
2021-06-15 09:00:30 -0500

    Clase 5 terminada

--- components/scripts/generate-custom-properties.js
+++ components/scripts/generate-custom-properties.js
@@ -10,18 +10,53 @@ const getValue = function (object) {
 
 const buildCustomProperties = function () {
 	const choicesKeys = Object.keys(choices);
-	console.log('>>>>>choices keys', choicesKeys);
-	const choicesCustomProperties = choicesKeys.reduce((prev, curr) => {
-		return choices[curr];
-	}, '');
-	console.log(choicesCustomProperties);
+	// console.log('>>>>>choices keys', choicesKeys);
+
+	// const choicesCustomProperties = choicesKeys.reduce((prev, curr) => {
+	// 	return choices[curr];
+	// }, '');
+	// console.log(choicesCustomProperties);
+
+	let choicesstring = '';
+
+	if (typeof choices['colors'] === 'object') {
+		const colorkeys = Object.keys(choices['colors']);
+
+		choicesstring = colorkeys.reduce((prev, curr) => {
+			if (typeof choices['colors'][curr] === 'object') {
+				const brandkeys = Object.keys(choices['colors'][curr]);
+				const colorStr = brandkeys.reduce((prevBrandKeys, currBrandKeys) => {
+					const value = choices['colors'][curr][currBrandKeys];
+					console.log('>>>', value);
+					return `
+					${prevBrandKeys}
+					--colors-${curr}-${currBrandKeys}: ${value};`;
+				}, '');
+				return `
+				${prev}
+				${colorStr}
+				`;
+
+				return brandkeys;
+			} else {
+				return `
+				${prev}
+				--colors-${curr}: ${choices['colors'][curr]};
+				`;
+			}
+			console.log(choices['colors'][curr]);
+		}, '');
+	}
+
+	const customProperties = choicesstring;
+
 	const data = `
 :root {
-	${buildCustomProperties}
+	${customProperties}
 }
 `;
 
-	fs.writeFile('./tokens.css', data, 'utf8', error => {
+	fs.writeFile('../tokens.css', data, 'utf8', error => {
 		if (error) {
 			return console.error(error);
 		}
